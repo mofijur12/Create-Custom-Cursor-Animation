def503b7e348967da2b2e79831a45aebe3fa8ddd
mofijur12 <mrkhan55679@gmail.com>
2024-09-26 15:57:48 +0600

    Cursor Animation Done

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-# Create-Custom-Cursor-Animation
+
+let main = document.querySelector("#main");
+let cursor = document.querySelector("#cursor");
+let image = document.querySelector("#image");
+
+main.addEventListener("mousemove",function (dets) {
+    gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y,
+        duration:0.8,
+    })
+})
+
+image.addEventListener("mouseenter",function (dets) {
+    cursor.innerHTML = "View More"
+    gsap.to(cursor,{
+        scale:4,
+        backgroundColor:"#ffffff9c"
+    })
+})
+image.addEventListener("mouseleave",function (dets) {
+    cursor.innerHTML = ""
+    gsap.to(cursor,{
+        scale:1,
+                backgroundColor:"#fff"
+    })
+})
